3d5311927ba5105a15aabec7da465f7c1a390f71
Benosu998 <46503231+Benosu998@users.noreply.github.com>
2019-06-01 12:40:01 +0300

    ignore

---
+++ javascript/newRecipe.js
@@ -0,0 +1,57 @@
+var i = 1;
+
+function addNewLine() {
+
+    var tableRef = document.getElementById("ingredientsTable");
+    var input = document.getElementById("input");
+    var ingredientName = document.getElementsByName("ingredientName")[0];
+    var ingredientQuantity = document.getElementsByName("ingredientQuantity")[0];
+    var ingredientMeasure = document.getElementsByName("ingredientMeasurement")[0];
+    var line = document.createElement('tr');
+    var nume = document.createElement('td');
+    var quantity = document.createElement('td');
+    var measure = document.createElement('td');
+    var button = document.createElement('td');
+    var buttonDelete = document.createElement('input');
+    buttonDelete.type = "submit";
+    buttonDelete.value = "delete";
+    buttonDelete.onclick = ev => {
+        deleteLine();
+        return false;
+    };
+    button.append(buttonDelete);
+    var inpNume = document.createElement('input');
+    inpNume.value = ingredientName.value;
+    inpNume.type = "text";
+    inpNume.readOnly = true;
+    inpNume.name = "NumeIngredient" + i;
+    var inpQuantity = document.createElement('input');
+    inpQuantity.value = ingredientQuantity.value;
+    inpQuantity.type = "text";
+    inpQuantity.readOnly = true;
+    inpQuantity.name = "Quantity" + i;
+    var inpMeasure = document.createElement('input');
+    inpMeasure.value = ingredientMeasure.value;
+    inpMeasure.type = "text";
+    inpMeasure.readOnly = true;
+    inpMeasure.name = "Measure" + i;
+    i++;
+    nume.append(inpNume);
+    quantity.append(inpQuantity);
+    measure.append(inpMeasure);
+    ingredientName.value = null;
+    ingredientQuantity.value = null;
+    ingredientMeasure.value = null;
+    line.append(nume);
+    line.append(quantity);
+    line.append(measure);
+    line.append(button);
+    tableRef.append(line);
+    tableRef.append(input);
+}
+
+function deleteLine() {
+    tableRef = document.getElementById("ingredientsTable");
+    currentLine = document.activeElement.parentNode.parentNode;
+    tableRef.removeChild(currentLine);
+}
--- pageCod/page/newRecipe.php
+++ pageCod/page/newRecipe.php
@@ -1,60 +1,70 @@
 <!DOCTYPE html>
 <html lang="EN">
-    <head>
-        <title>New Recipe</title>
-        <link rel="shortcut icon" type="image/x-icon" href="../../image/logo.png" />
-        <link rel="stylesheet" type="text/css" href="../css/newRecipe.css">
-    </head>
-    <body>
-        
-            <header>
-                    <img src="../../image/banner-2.0.png" alt="Big-Logo">
-            </header>
-            <?php
-                include "../phpFile/view.php";
-                $view=new view();
-                $view->start(0);
-                if(isset($_COOKIE['username'])){
-                    $view->index1PHP(0);
-                    $view->addNewPHP(3);
-                    $view->profilePHP(3);
-                    $view->calculatorPHP(3);
-                    $view->manualPHP(3);
-                    $view->logout(0);
-                } else {
-                    setcookie("accesinterzis",1,time() + 3,"/");
-                    header("Location: ../../index.php");
-                }
-                $view->start(2);
-            ?>
-            <div class="form">
-                    <h2>Add New Recipe</h2>
-                    <input name="RecipeName" placeholder="Recipe Name" type="text"/>
-                    <div class="ingredients">
-                        <table>
-                        <tr>
-                            <th>ingredient</th>
-                            <th>cantitate</th>
-                            <th>Unitate Masura</th>
-                            <th>Action</th>
-                            
-                        </tr>
-                        <tr>
-                            <td><input name="ingredientName" placeholder="Ingredient" type="text"/></td>
-                            <td><input name="ingredientQuantity" placeholder="Quantity" type="text"/></td>
-                            <td><input name="ingredientMeasurement" placeholder="Measurement Unit" type="text"/></td>
-                            <td><input type="submit" value="Add Ingredient"/></td>
-                        </tr>
-                        <tr>
-                            <td>Apa</td>
-                            <td>2</td>
-                            <td>Litri</td>
-                            <td><input type="submit" value="delete"/></td>
-                        </tr>
-                        </table>
-                        <p>Total Calories:<a>0</a></p>
-                    </div>
-                    <input type="submit" value="Add Recipe"/>
-                </div>
-    </body>
+<head>
+    <title>New Recipe</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../../image/logo.png"/>
+    <link rel="stylesheet" type="text/css" href="../css/newRecipe.css">
+    <script src="../../javascript/newRecipe.js"></script>
+</head>
+<body>
+
+<header>
+    <img src="../../image/banner-2.0.png" alt="Big-Logo">
+</header>
+<?php
+include "../phpFile/view.php";
+$view = new view();
+$view->start(0);
+if (isset($_COOKIE['username'])) {
+    $view->index1PHP(0);
+    $view->addNewPHP(3);
+    $view->profilePHP(3);
+    $view->calculatorPHP(3);
+    $view->manualPHP(3);
+    $view->logout(0);
+} else {
+    setcookie("accesinterzis", 1, time() + 3, "/");
+    header("Location: ../../index.php");
+}
+$view->start(2);
+?>
+<div class="form">
+    <h2>Add New Recipe</h2>
+    <form method="post" action="newRecipe.php">
+        <input name="RecipeName" placeholder="Recipe Name" type="text"/>
+        <div class="ingredients">
+            <table id="ingredientsTable">
+                <tr>
+                    <th>ingredient</th>
+                    <th>cantitate</th>
+                    <th>Unitate Masura</th>
+                    <th>Action</th>
+
+                </tr>
+                <tr id="input">
+                    <td><input name="ingredientName" placeholder="Ingredient" type="text"/></td>
+                    <td><input name="ingredientQuantity" placeholder="Quantity" type="text"/></td>
+                    <td><input name="ingredientMeasurement" placeholder="Measurement Unit" type="text"/></td>
+                    <td><input type="submit" value="Add Ingredient" name="addIngredient"
+                               onclick="addNewLine();return false;"/></td>
+                </tr>
+            </table>
+            <p>Total Calories:0</p>
+        </div>
+        <input type="submit" value="Add Recipe" name="AddRecipe"/>
+    </form>
+</div>
+
+</body>
+
+
+<?php
+if (isset($_POST['AddRecipe'])) {
+    foreach ($_POST as $key => $value) {
+        if (strpos($key, 'NumeIngredient') === 0) {
+            echo '<script>console.log("'.$value.'")</script>';
+        }
+    }
+}
+?>
 </html>
